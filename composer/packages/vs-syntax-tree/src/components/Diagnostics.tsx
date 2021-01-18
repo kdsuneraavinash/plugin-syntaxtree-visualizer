f92958971d59e3190f20f24bd19093a184bcc648
SumayyaZiyad <sumayyaziyad034@gmail.com>
2021-01-18 17:08:52 +0530

    Diagnostics Popup revamp + Color changes

--- composer/packages/vs-syntax-tree/src/components/Diagnostics.tsx
+++ composer/packages/vs-syntax-tree/src/components/Diagnostics.tsx
@@ -1,32 +1,60 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { TreeNodeDetailsProps } from "../tree-interfaces";
 
 function Diagnostics(props: TreeNodeDetailsProps) {
+    const [isEdgeNode, updateIsEdgeNode] = useState(false);
+    const [isBottomNode, updateIsBottomNode] = useState(false);
+
+    useEffect(() => {
+        if (props.node.x + 400 > window.innerWidth) {
+            updateIsEdgeNode(true);
+        }
+
+        if (props.node.y + 300 > window.innerHeight) {
+            updateIsBottomNode(true);
+        }
+    }, []);
+
     return (
-        <div
-            style = {{
-                backgroundColor: "#faf3c0",
-                borderRadius: 5,
-                left: props.node.x > window.innerWidth - 250 ?
-                    props.node.x - 130 :
-                    props.node.x + (props.node.width / 1.25),
-                minWidth: 160,
-                padding: 10,
-                position: "absolute",
-                textAlign: "left",
-                top: props.node.y > window.innerHeight - 150 ?
-                    props.node.y - 100 :
-                    props.node.y + (props.node.height / 1.25),
-                zIndex: 1
-            }}
-        >
-            <p> <b>This block contains :</b></p> <hr/>
-            {props.node.diagnostics.map((item, id) => {
-                    return <p key = {id}>
-                        {item.message}
-                    </p>;
-                })
-            }
+        <div>
+            <div
+                style = {{
+                    borderBottom: isBottomNode ? "none" : "15px solid #ffeee6",
+                    borderLeft: "7.5px solid transparent",
+                    borderRight: "7.5px solid transparent",
+                    borderTop: isBottomNode ? "15px solid #ffeee6" : "none",
+                    height: 0,
+                    left: props.node.x + props.node.width - 25,
+                    position: "absolute",
+                    top: isBottomNode ? props.node.y - 10 : props.node.y + 45,
+                    transform: "translateX(-50%)",
+                    width: 0
+                }}
+            />
+
+            <div
+                style = {{
+                    backgroundColor: "#ffeee6",
+                    borderRadius: 5,
+                    left: props.node.x + props.node.width - 40,
+                    minWidth: 160,
+                    padding: 10,
+                    position: "absolute",
+                    textAlign: "left",
+                    top: isBottomNode ? props.node.y - 10 : props.node.y + props.node.height + 10,
+                    transform: isEdgeNode ? (isBottomNode ? "translate(-100%, -100%)" : "translateX(-100%)") :
+                        (isBottomNode ? "translateY(-92.5%)" : "translateX(-1%)"),
+                    zIndex: 1
+                }}
+            >
+                <p> <b>This block contains :</b></p> <hr/>
+                {props.node.diagnostics.map((item, id) => {
+                        return <p key = {id}>
+                            {item.message}
+                        </p>;
+                    })
+                }
+            </div>
         </div>
     );
 }
--- composer/packages/vs-syntax-tree/src/components/NodeDetails.tsx
+++ composer/packages/vs-syntax-tree/src/components/NodeDetails.tsx
@@ -3,20 +3,15 @@ import { Minutiae, TreeNodeDetailsProps } from "../tree-interfaces";
 
 function NodeDetails(props: TreeNodeDetailsProps) {
     const [isEdgeNode, updateIsEdgeNode] = useState(false);
-    const [isTopNode, updateIsTopNode] = useState(false);
     const [isBottomNode, updateIsBottomNode] = useState(false);
 
     useEffect(() => {
-        if (props.node.x + 400 > window.innerWidth) {
+        if (props.node.x + 425 > window.innerWidth) {
             updateIsEdgeNode(true);
         }
 
-        if (props.node.y + 350 > window.innerHeight) {
+        if (props.node.y + 300 > window.innerHeight) {
             updateIsBottomNode(true);
-        }
-
-        if (props.node.y < 150) {
-            updateIsTopNode(true);
         }
     }, []);
 
@@ -33,22 +28,22 @@ function NodeDetails(props: TreeNodeDetailsProps) {
             <div
                 style = {{
                     borderBottom: "7.5px solid transparent",
-                    borderLeft: isEdgeNode ? "15px solid #FFE5B4" : "none",
-                    borderRight: isEdgeNode ? "none" : "15px solid #FFE5B4",
+                    borderLeft: isEdgeNode ? "15px solid #FFFDD0" : "none",
+                    borderRight: isEdgeNode ? "none" : "15px solid #FFFDD0",
                     borderTop: "7.5px solid transparent",
                     height: 0,
                     left: isEdgeNode ? props.node.x - 15 :
                         props.node.x + props.node.width,
                     position: "absolute",
-                    top: props.node.y + (props.node.height / 2),
+                    top: props.node.y + (props.node.height / 1.1),
                     transform: "translateY(-50%)",
                     width: 0
                 }}
             />
             <div
                 style = {{
-                    backgroundColor: "#FFE5B4",
-                    borderRadius: 5,
+                    backgroundColor: "#FFFDD0",
+                    borderRadius: 8,
                     left: isEdgeNode ? props.node.x - 15 :
                         props.node.x + props.node.width + 15,
                     minHeight: 190,
@@ -56,10 +51,9 @@ function NodeDetails(props: TreeNodeDetailsProps) {
                     padding: 10,
                     position: "absolute",
                     textAlign: "left",
-                    top: props.node.y + (props.node.height / 2),
-                    transform: isEdgeNode ? (isBottomNode ? "translate(-100%, -92.5%)" :
-                        (isTopNode ? "translate(-100%, -10%)" : "translate(-100%, -50%)")) :
-                        (isBottomNode ? "translateY(-92.5%)" : (isTopNode ? "translateY(-10%)" : "translateY(-50%)")),
+                    top: props.node.y + (props.node.height / 1.15),
+                    transform: isEdgeNode ? (isBottomNode ? "translate(-87.5%, -92.5%)" : "translate(-100%, -6%)") :
+                        (isBottomNode ? "translateY(-87.5%)" : "translateY(-6%)"),
                     zIndex: 1
                 }}
             >
