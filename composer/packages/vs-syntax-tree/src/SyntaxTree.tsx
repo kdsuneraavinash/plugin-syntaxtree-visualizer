fd96162e45bd37d5b3279d1f82f6a0991e346168
SumayyaZiyad <sumayyaziyad034@gmail.com>
2021-01-19 14:48:45 +0530

    Tree representation switch init

--- composer/packages/vs-syntax-tree/src/SyntaxTree.tsx
+++ composer/packages/vs-syntax-tree/src/SyntaxTree.tsx
@@ -1,8 +1,10 @@
 import React, {useEffect, useState} from "react";
+import { Dimmer, Loader, Radio } from "semantic-ui-react";
 import GraphicalSyntaxTree from "./graphical-syntaxtree";
 import { SyntaxTreeProps, TreeGraph } from "./tree-interfaces";
 
 function SyntaxTree(props: SyntaxTreeProps) {
+    const [isGraphicalView, updateIsGraphicalView] = useState(false);
     const [syntaxTreeGraph, setSyntaxTreeGraph] = useState<TreeGraph | undefined>(undefined);
 
     useEffect(() => {
@@ -11,11 +13,51 @@ function SyntaxTree(props: SyntaxTreeProps) {
         });
     }, [props]);
 
+    function updateView() {
+        updateIsGraphicalView(!isGraphicalView);
+    }
+
     return (
-        <div>
-            {syntaxTreeGraph &&
-                <GraphicalSyntaxTree treeGraph = {syntaxTreeGraph} onCollapseTree = {props.onCollapseTree} />
-            }
+        <div
+            style = {{
+                position: "relative"
+            }}
+        >
+            <div
+                style = {{
+                    display: "flex",
+                    flexDirection: "row",
+                    fontSize: "15px",
+                    marginLeft: "2%"
+                }}
+            >
+                <p style={{paddingRight: "15px"}}>Graphical Tree View</p>
+                <Radio toggle onChange = {updateView} checked = {isGraphicalView} />
+            </div>
+
+            <div>
+                {!isGraphicalView && syntaxTreeGraph &&
+                    <div>
+                        <text>This is where the drop down happens!</text>
+                    </div>
+                }
+
+                {isGraphicalView && syntaxTreeGraph &&
+                    <div
+                        style = {{
+                            position: "relative"
+                        }}
+                    >
+                        <GraphicalSyntaxTree treeGraph = {syntaxTreeGraph} onCollapseTree = {props.onCollapseTree} />
+                    </div>
+                }
+
+                {!syntaxTreeGraph &&
+                    <Dimmer inverted>
+                        <Loader size="medium">Loading</Loader>
+                    </Dimmer>
+                }
+            </div>
         </div>
     );
 }
