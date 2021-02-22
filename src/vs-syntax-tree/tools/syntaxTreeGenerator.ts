32862eea81bc676f2e3eaf61dc62d58d31cd1f1f
SumayyaZiyad <sumayyaziyad034@gmail.com>
2021-02-22 18:21:45 +0530

    Remove log

--- src/vs-syntax-tree/tools/syntaxTreeGenerator.ts
+++ src/vs-syntax-tree/tools/syntaxTreeGenerator.ts
@@ -17,7 +17,6 @@ export function retrieveGraph(responseTree: any) {
 }
 
 export function updateSyntaxTree(nodeID: string, isGraphical: boolean) {
-    console.log(JSON.stringify(syntaxTreeObj, null, 2));
     if (isGraphical) {
         nodeEdges = []; nodeMembers = [];
     }
