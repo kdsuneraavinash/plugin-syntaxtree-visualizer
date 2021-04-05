66ea8ae3e8214a6832cf2c633affb4430d566fa2
SumayyaZiyad <sumayyaziyad034@gmail.com>
2021-04-06 00:18:52 +0530

    LS suspend updatte

--- src/server/server.ts
+++ src/server/server.ts
@@ -39,7 +39,7 @@ export function getServerOptions(ballerinaCmd: string): Executable {
         debug("Language Server is starting in debug mode.");
         const debugPort = 5005;
         opt.env.BAL_JAVA_DEBUG = debugPort;
-        opt.env.BAL_DEBUG_OPTS = "-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=" + debugPort + ",quiet=y";
+        opt.env.BAL_DEBUG_OPTS = "-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=" + debugPort + ",quiet=y";
     }
 
     if (process.env.LS_CUSTOM_CLASSPATH) {
